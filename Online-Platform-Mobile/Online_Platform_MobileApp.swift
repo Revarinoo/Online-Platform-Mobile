20b73ec2adafcd65fe05a16921b93b3e2d308d41
revarinoo <revarinoputra@gmail.com>
2021-10-09 11:01:36 +0700

    SetUp Folder Structure

---
+++ Online-Platform-Mobile/Online_Platform_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_Platform_MobileApp.swift
+//  Online-Platform-Mobile
+//
+//  Created by Revarino Putra on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Online_Platform_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
