d19ea9bbac1192ceeafcc167ddbe5a63e17749c1
EcoRanger <adiputrachristian17@gmail.com>
2021-12-01 20:50:51 +0700

    payment-component

---
+++ Online-Platform-Mobile/Cores/Features/Payment/Payment+View/PaymentView.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentView.swift
+//  Online-Platform-Mobile
+//
+//  Created by Christian Adiputra on 01/12/21.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentView()
+    }
+}
