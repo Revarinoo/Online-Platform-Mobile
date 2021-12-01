65dd057b5b7d78801b91d7581af4a0227f0e2e52
EcoRanger <adiputrachristian17@gmail.com>
2021-12-01 23:54:18 +0700

    payment

--- Online-Platform-Mobile/Cores/Features/Payment/Payment+View/PaymentView.swift
+++ Online-Platform-Mobile/Cores/Features/Payment/Payment+View/PaymentView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct PaymentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            VStack(alignment: .leading) {
+                Text("Payment time")
+                    .foregroundColor(Color.theme.primary)
+                    .fontWeight(.semibold)
+                PaymentTime()
+                    .padding()
+                    .frame(width: 358, alignment: .top)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 5, x: 2, y: 2)
+            }
+            VStack(alignment: .leading) {
+                Text("Transfer to")
+                    .foregroundColor(Color.theme.primary)
+                    .fontWeight(.semibold)
+                Transfer()
+                    .padding()
+                    .frame(width: 358, height: 167, alignment: .top)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.2), radius: 5, x: 2, y: 2)
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
