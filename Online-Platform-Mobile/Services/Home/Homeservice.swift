92aeed63bb82059f6e767357a644203add79cf82
revarinoo <revarinoputra@gmail.com>
2022-01-13 15:59:18 +0700

    Readjust recommendation endpoint

--- Online-Platform-Mobile/Services/Home/Homeservice.swift
+++ Online-Platform-Mobile/Services/Home/Homeservice.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Homeservice {
+    @AppStorage("JWT", store: .standard) var token = ""
     
     func getCategories(completionHandler: @escaping (_ result: CategoryResponse?)-> Void){
         
@@ -19,7 +21,8 @@ class Homeservice {
     
     func getRecommendedSeller(completionHandler: @escaping (_ result: RecommendedSellerResponse?)-> Void) {
         
-        let request = NSMutableURLRequest(url: NSURL(string: HttpService.endpoint + "recommendation/seller")! as URL)
+        let request = NSMutableURLRequest(url: NSURL(string: HttpService.endpoint + "product/recommendation")! as URL)
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         HttpService.shared.request(request as URLRequest, resultType: RecommendedSellerResponse.self) { response in
             _ = completionHandler(response)
         }
